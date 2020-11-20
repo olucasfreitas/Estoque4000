e91e817457bec73422bba9c36f46023527bbbd30
Elielson68 <elielsonbr.com@gmail.com>
2020-11-19 21:40:40 -0300

    feat: function to calculate the sold_price and maximum_profit

--- estoque/blueprints/view.py
+++ estoque/blueprints/view.py
@@ -1,17 +1,31 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from ..extensions.serializer import ManufacturerSchema, ProductSchema
-from ..extensions.database import Session, Manufacturer
+from ..extensions.database import Session
+from ..extensions.database import Manufacturer
+from ..extensions.database import Product
+from sympy import var, diff, Lambda, solve, init_printing
 
+def lucro_max(Q, C):
+  """
+  Q = Quantidade
+  C = Custo
+  L(x) = (Q - x) * x - C(Q - x)
+  L(x)= Q.x - x² - C.Q + C.x
+  :param custo:
+  :param venda:
+  :return:
+  """
+  init_printing()
+  var('x,y')
+  f = Lambda(x, (- x ** 2 + (Q+C)*x - C*Q))
+  print("Expressão: " ,f'- x² + {Q+C}.x - {C*Q}')
+  derivada = diff(f(x), x)
+  print("Derivada: ", )
+  preco = solve(derivada)[0]
+  print("Preço para obter lucro máximo: ", preco)
+  lucro = -preco**2 + (Q+C)*preco - C*Q
+  return preco, lucro
 bp = Blueprint('view', __name__, template_folder='templates', static_folder='static')
-
-def lucro_max(quantidade_produto, custo_produto ):
-  # Q = Quantidade
-  # C = Custo
-  # L(x) = (Q - x) * x - C(Q - x)
-  # L(x)= Q.x - x² - C.Q + C.x
-  ...
-
-
 
 @bp.route('/produce', methods=['GET', 'POST'])
 def produce():
@@ -20,6 +34,9 @@ def produce():
   else:
     product = ProductSchema()
     product_info = request.form.to_dict()
+    sold_price, lucro = lucro_max(int(product_info['quantity']), int(product_info['production_cost']))
+    product_info["sold_price"] = sold_price
+    product_info["maximum_profit"] = lucro
     product_load = product.load(product_info)
     Session.add(product_load)
     Session.commit()
@@ -28,7 +45,7 @@ def produce():
 @bp.route('/list_produtcs')
 @bp.route('/')
 def list_produtcs():
-  return render_template('lista.html')
+  return render_template('lista.html', produtos=Product.query.all())
 
 def init_app(app):
   app.register_blueprint(bp)
--- estoque/extensions/database.py
+++ estoque/extensions/database.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 Session = db.session
+
 def init_app(app):
     db.init_app(app)
     db.create_all(app=app)
@@ -13,11 +14,18 @@ class Manufacturer(db.Model):
     name = db.Column(db.String(150))
     stock = db.relationship("Stock", backref="manufacturer", lazy="select")
 
+    def __init__(self, name, stock):
+        self.name = name
+        self.stock = stock
+
 class Stock(db.Model):
     __name__ = "stock"
     id = db.Column(db.Integer, primary_key=True)
     manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturer.id'))
     products = db.relationship("Product", backref="stock", lazy="select")
+
+    def __init__(self, products):
+        self.products = products
 
 class Product(db.Model):
     __name__ = "product"
@@ -26,4 +34,12 @@ class Product(db.Model):
     stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'))
     production_cost = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    sold_price = db.Column(db.String(150), nullable=True)
+    sold_price = db.Column(db.Float, nullable=True)
+    maximum_profit = db.Column(db.Float, nullable=True)
+
+    def __init__(self, name, production_cost, quantity, sold_price=None, maximum_profit=None):
+        self.name = name
+        self.production_cost = production_cost
+        self.quantity = quantity
+        self.sold_price = sold_price
+        self.maximum_profit = maximum_profit
