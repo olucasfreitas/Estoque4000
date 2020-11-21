f2e241a45a18240ca9e14a6f9eda65bc1e960de0
Elielson68 <elielsonbr.com@gmail.com>
2020-11-20 23:36:25 -0300

    docs: lucro function with docstring

--- estoque/blueprints/view.py
+++ estoque/blueprints/view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from ..extensions.serializer import ManufacturerSchema, ProductSchema
 from ..extensions.database import Session
 from ..extensions.database import Manufacturer
@@ -7,10 +7,47 @@ from sympy import var, diff, Lambda, solve, init_printing
 
 def lucro_max(Q, C):
   """
+  Usamos como base o seguinte exemplo para construir nossa função:
+
+  "Um fabricante pode produzir calçados ao custo de R$ 20,00 o par.
+   Estima-se que, se cada par for vendido por x reais, o fabricante venderá
+   por mês 80 – x (0 ≤ x ≤ 80) pares de sapatos. Assim, o lucro mensal do fabricante
+   é uma função do preço de venda. Qual deve ser o preço de venda, de modo que o lucro
+   mensal seja máximo?"
+
+   Precisamos ficar atento para que:
+
+   Q = Quantidade
+   x = Preço do produto a venda.
+   0 ≤ x ≤ Q
+   Logo, nosso preço precisa ser abaixo da quantidade.
+
+   Também precisamos ficar atento para:
+
+   Q - x = É o número que o fabricante venderá por mês para que obtenha o lucro máximo, logo,
+   Se por exemplo o preço de um produto for de 10 reais e vendessemos 30 unidades dele.
+   Q = Quantidade, 
+   Q = 30
+   x = Preço
+   x = 10
+   Q - x = 30 - 10 = 20.
+   20 é o número de unidades que precisaria ser vendida por mês para que se obtesse o lucro máximo com
+   o preço de 10 reais.
+
+
+
+  C = Custo de produção
   Q = Quantidade
-  C = Custo
-  L(x) = (Q - x) * x - C(Q - x)
-  L(x)= Q.x - x² - C.Q + C.x
+  x = Preço de venda
+  Função custo:
+    C(x) = C(Q - x)
+  Função receita:
+    R(x) = (Q - x) * x
+  Função lucro:
+    L(x) = R(x) - C(x)
+  Logo, temos:
+    L(x) = (Q - x) * x - C(Q - x)
+    L(x)= Q.x - x² - C.Q + C.x
   :param custo:
   :param venda:
   :return:
@@ -34,7 +71,11 @@ def produce():
   else:
     product = ProductSchema()
     product_info = request.form.to_dict()
-    sold_price, lucro = lucro_max(int(product_info['quantity']), int(product_info['production_cost']))
+    sold_price, lucro = lucro_max(float(product_info['quantity']), float(product_info['production_cost']))
+    if int(sold_price) > int(product_info["quantity"]):
+      flash("Para obter o lucro máximo com seu preço, o seu preço deve ser menor que a quantidade.\n"
+            "Sua quantidade {}, preço encontrado {}".format(product_info["quantity"], sold_price), "danger")
+      return redirect(url_for("view.produce"))
     product_info["sold_price"] = sold_price
     product_info["maximum_profit"] = lucro
     product_load = product.load(product_info)
