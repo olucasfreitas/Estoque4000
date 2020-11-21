3fa66f0c0061767078c0627dbb18435b280ce38d
Elielson68 <elielsonbr.com@gmail.com>
2020-11-20 23:37:36 -0300

    feat: one-to-one relationship applied

--- estoque/extensions/database.py
+++ estoque/extensions/database.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 Session = db.session
+
 
 def init_app(app):
     db.init_app(app)
@@ -9,11 +11,21 @@ def init_app(app):
     app.db = db
 
 
+
+class Manufacturer(db.Model):
+    __name__ = "manufacturer"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    stock = db.relationship("Stock", uselist=False, backref="manufacturer")
+    
+    def __init__(self, name):
+        self.name = name
+
 class Stock(db.Model):
     __name__ = "stock"
     id = db.Column(db.Integer, primary_key=True)
-    manufacturer = db.relationship("manufacturer", backref="stock", lazy="select")
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturer.id'), unique=True)
+    products = db.relationship("Product", backref="stock", lazy="select")
 
     def __init__(self, products):
         self.products = products
@@ -22,11 +34,11 @@ class Product(db.Model):
     __name__ = "product"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
-    stock = db.relationship("Stock", backref="product", lazy="select")
     production_cost = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     sold_price = db.Column(db.Float, nullable=True)
     maximum_profit = db.Column(db.Float, nullable=True)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'))
 
     def __init__(self, name, production_cost, quantity, sold_price=None, maximum_profit=None):
         self.name = name
@@ -35,11 +47,3 @@ class Product(db.Model):
         self.sold_price = sold_price
         self.maximum_profit = maximum_profit
 
-class Manufacturer(db.Model):
-    __name__ = "manufacturer"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150))
-    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'))
-
-    def __init__(self, name):
-        self.name = name
